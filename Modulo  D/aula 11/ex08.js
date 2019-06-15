8d5d4b388f514390ecd831489db5f48de1b8fba5
Your Name <you@example.com>
2019-06-15 03:54:10 -0300

    Condições Parte 1

---
+++ Modulo  D/aula 11/ex08.js
@@ -0,0 +1,9 @@
+var velocidade = 50
+
+console.log(`A sua velocidade é: ${velocidade}`)
+
+if(velocidade > 60){
+    console.log('Você foi multado !')
+}
+
+console.log('Dirija sempre com cuidado !s')
---
+++ Modulo  D/aula 11/ex09.js
@@ -0,0 +1,8 @@
+var pais = "Brasil"
+
+if (pais == "Brasil"){
+    console.log('Terra de Gente boa ')
+}
+else{
+    console.log("Aí tem que ver...")
+}
