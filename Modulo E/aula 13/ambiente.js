4a958759c1563349c7754ffa72b389bc5fe83c0e
Your Name <you@example.com>
2019-06-16 21:00:10 -0300

    Modulo E - Repetições em Js

---
+++ Modulo E/aula 13/ambiente.js
@@ -0,0 +1,19 @@
+// console.log("tudo bem ?")
+// console.log("tudo bem ?")
+// console.log("tudo bem ?")
+// console.log("tudo bem ?")
+// console.log("tudo bem ?")
+// console.log("tudo bem ?")
+
+var c1 = 0
+var c2 = 0
+
+while( c1 <=  500){
+    console.log(` Testando o While ${c1} vezes !`)
+    c1 ++
+}
+
+do {
+    console.log(`Agora testando o Do While ${c2} vezes !`)
+    c2 ++
+}while(c2 <= 500)
